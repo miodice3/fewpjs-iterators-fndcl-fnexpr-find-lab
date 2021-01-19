03346f45264f553830285ccbd8648b4044352be3
Michael Iodice <miodice3@gmail.com>
2021-01-19 13:37:04 -0700

    commit tree 84cb7d8941ae20c424ae8521cf3c0d87d051b504

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+] 
+
+// function superbowlWin(argument){
+//   // console.log("this is the argument", argument)
+//   argument.find
+//   argument.forEach(e => console.log(e.result));
+
+// }
+
+// function superbowlWin(record){
+//   record.find( function(s) { 
+//     console.log("this is each iteration", s)
+//     return s.result === "W" })
+
+// }
+
+function superbowlWin(record){
+  if (record.find( function(s) { return s.result === "W" })){
+    return record.find( function(s) { return s.result === "W" }).year
+  } else {
+    return undefined
+  }
 }
